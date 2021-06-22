f4c2a4f9a0e2ffe1154071225e4e72958e25f056
MarlonMoreno <marlonmoreno894@gmail.com>
2021-06-21 19:32:48 -0500

    Add find and create functionality to Genre class

--- javascripts/genre.js
+++ javascripts/genre.js
@@ -6,5 +6,17 @@ class Genre {
         this.name = name
         Genre.all.push(this)
     }
+
+    static findByName(name) {
+        return this.all.find(function(genre) { genre.name === name})
+    }
+
+    static findById(id) {
+        return this.all.find(genre => genre.id === id)
+    }
+
+    static findOrCreateBy(movie) {
+        return this.findByName(movie.name) || new Genre(genre)
+    }
     
 }
